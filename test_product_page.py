d07b032d0e12e7ea18ab177eca37da8ad2c62bbe
Pavel Khmel <152857475+houston03@users.noreply.github.com>
2024-11-02 09:52:40 +0300

    test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -8,7 +8,7 @@ from .pages.basket_page import BasketPage
 link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
 
 
-# [0, 1, 2, 3, 4, 5, 6, pytest.param(7, marks=pytest.mark.xfail), 8, 9]
+
 @pytest.mark.need_review
 @pytest.mark.parametrize('suffix', (no if no != 7 else pytest.param(no, marks=pytest.mark.xfail) for no in range(10)))
 def test_guest_can_add_product_to_basket(browser, suffix):
